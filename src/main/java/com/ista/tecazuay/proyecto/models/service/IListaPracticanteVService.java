b9b414e9019ecc2fd8498c73768ff533e5b80e91
IgnacioJuan <juan.guachichullca.est@tecazuay.edu.ec>
2022-09-29 12:20:14 -0500

    Busqueda por convenio en la vista
    si

--- src/main/java/com/ista/tecazuay/proyecto/models/service/IListaPracticanteVService.java
+++ src/main/java/com/ista/tecazuay/proyecto/models/service/IListaPracticanteVService.java
@@ -2,10 +2,12 @@ package com.ista.tecazuay.proyecto.models.service;
 
 import java.util.List;
 
+import com.ista.tecazuay.proyecto.models.primary.entity.RealizaPractica;
 import com.ista.tecazuay.proyecto.models.primary.entity.lista_practicantes_view;
 
 public interface IListaPracticanteVService {
 	
 	public List<lista_practicantes_view> findAll();
+	public List<lista_practicantes_view> findbyconv(Long numconv);
 
 }
