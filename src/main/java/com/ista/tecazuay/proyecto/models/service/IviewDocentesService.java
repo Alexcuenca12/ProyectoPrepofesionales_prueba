397c328241eb569a85040f62cca6ceb28962c455
JoseEsp01 <jose.espinoza.est@tecazuay.edu.ec>
2022-09-29 23:32:07 -0500

    Controlador viewDocentes

---
+++ src/main/java/com/ista/tecazuay/proyecto/models/service/IviewDocentesService.java
@@ -0,0 +1,11 @@
+package com.ista.tecazuay.proyecto.models.service;
+
+import java.util.List;
+
+import com.ista.tecazuay.proyecto.models.primary.entity.viewDocentes;
+
+public interface IviewDocentesService {
+
+	public List<viewDocentes>findAll();
+
+}
