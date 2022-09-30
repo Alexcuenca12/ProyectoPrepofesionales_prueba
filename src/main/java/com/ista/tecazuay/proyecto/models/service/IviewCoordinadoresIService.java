178da313a71e6862cbabcecf40df3aaab4a5f6ad
JoseEsp01 <jose.espinoza.est@tecazuay.edu.ec>
2022-09-30 00:06:08 -0500

    Controlador viewCoordinadorI

---
+++ src/main/java/com/ista/tecazuay/proyecto/models/service/IviewCoordinadoresIService.java
@@ -0,0 +1,10 @@
+package com.ista.tecazuay.proyecto.models.service;
+
+import java.util.List;
+
+import com.ista.tecazuay.proyecto.models.primary.entity.viewCoordinadoresI;
+
+public interface IviewCoordinadoresIService {
+
+	public List<viewCoordinadoresI>findAll();
+}
