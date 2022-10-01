12e21e0106c6321f69f74fac8b9957ac33862677
BryanCabrera09 <81339678+BryanCabrera09@users.noreply.github.com>
2022-10-01 15:09:55 -0500

    Update
    Roles

--- src/main/java/com/ista/tecazuay/proyecto/models/primary/entity/viewCoordinadoresI.java
+++ src/main/java/com/ista/tecazuay/proyecto/models/primary/entity/viewCoordinadoresI.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Table(name = "verdocente")
+@Table(name = "viewcoordinadoresi")
 @NoArgsConstructor
 @AllArgsConstructor
 public class viewCoordinadoresI {
